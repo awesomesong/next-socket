ba8b9993bb851d9e449eb76841843741dd0fe047
songheekang <kshs24820@gmail.com>
2025-03-30 22:09:14 +0900

    fix: export 할때 누락된 default 추가

--- src/app/lib/cache.ts
+++ src/app/lib/cache.ts
@@ -2,7 +2,7 @@ import { unstable_cache as nextCache } from "next/cache"
 import { cache as reactCache } from "react"
 
 type Callback = (...args: any[]) => Promise<any>
-export function cache<T extends Callback>(
+export default function cache<T extends Callback>(
   cb: T,
   keyParts: string[],
   options: { revalidate?: number | false; tags?: string[] } = {}
