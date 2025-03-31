f52cb3a300b0b03167eb7f70fdcaa46e0ca0b47b
songheekang <kshs24820@gmail.com>
2025-03-31 16:38:35 +0900

    fix: ApolloClient의 credentials 삭제

--- src/app/context/ApolloProviders.tsx
+++ src/app/context/ApolloProviders.tsx
@@ -4,7 +4,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
     uri: `${process.env.NEXT_PUBLIC_URL}/api/graphql`,
     cache: new InMemoryCache(),
-    credentials: "include", 
 })
 
 const ApolloProviders = ({ children }: { children: React.ReactNode } ) => {
