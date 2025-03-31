a235b939a93657bef3aa9fcdf6af207d3809b6cb
songheekang <kshs24820@gmail.com>
2025-03-31 16:54:50 +0900

    fix: apollo client credentials 추가

--- src/app/context/ApolloProviders.tsx
+++ src/app/context/ApolloProviders.tsx
@@ -4,6 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
     uri: `${process.env.NEXT_PUBLIC_URL}/api/graphql`,
     cache: new InMemoryCache(),
+    credentials: "include", 
 })
 
 const ApolloProviders = ({ children }: { children: React.ReactNode } ) => {
