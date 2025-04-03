7630031b132af7393650e6d454d30783eace40b9
songheekang <kshs24820@gmail.com>
2025-04-03 10:20:40 +0900

    fix: props type 설정

--- next-server/src/app/lib/createBlogsComments.ts
+++ next-server/src/app/lib/createBlogsComments.ts
@@ -1,4 +1,9 @@
-export const createBlogsComments = async ({blogId, comment}) => {
+type Props = {
+    blogId: string;
+    comment: string;
+}
+
+export const createBlogsComments = async ({blogId, comment}: Props) => {
     const res = await fetch(`/api/blogs/comments/${blogId}`, {
         method: 'POST',
         headers: {
