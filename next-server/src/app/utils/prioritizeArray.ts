c21c05639cf28e7b25d7f47937e3ba1970e979bf
songheekang <kshs24820@gmail.com>
2025-04-03 09:14:16 +0900

    refactor: file 재설정

--- next-server/src/app/utils/prioritizeArray.ts
+++
@@ -1,7 +0,0 @@
-type Item = { [key: string]: any };
-
-export const prioritizeArray = <T extends Item>(arr: T[], key: keyof T, value: any): T[] => {
-  const matched = arr?.filter(item => item[key] === value);
-  const others = arr?.filter(item => item[key] !== value);
-  return matched ? [...matched, ...others] : [];
-};
