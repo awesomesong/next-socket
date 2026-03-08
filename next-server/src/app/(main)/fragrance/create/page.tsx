d783640f594021e3d7e0c692676bcd977a90370a
songheekang <kshs24820@gmail.com>
2026-03-08 19:40:15 +0900

    fix: create/page.tsx의 향수 정보 추가 글자 가운데 정렬

--- next-server/src/app/(main)/fragrance/create/page.tsx
+++ next-server/src/app/(main)/fragrance/create/page.tsx
@@ -3,8 +3,8 @@ import FormFragrance from '@/src/app/components/FormFragrance';
 const FragranceCreatePage = () => {
     return (
         <div className='px-4 pt-8 pb-4 md:p-8 max-w-[1440px] mx-auto min-h-screen'>
-            <h2 className="text-top">
-                <span className="text-gradient-scent text-center">향수 정보 추가</span>
+            <h2 className="text-top text-center">
+                <span className="text-gradient-scent">향수 정보 추가</span>
             </h2>
             <FormFragrance isEdit={false} />
         </div>
