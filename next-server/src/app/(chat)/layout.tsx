caf04a1b6a95877112741c6e99a4aa0f68e2eae7
songheekang <kshs24820@gmail.com>
2026-03-24 17:08:47 +0900

    refactor: 하위 경로에서 덮어씌워져 사용되지 않는 메타데이터 코드 정리

--- next-server/src/app/(chat)/layout.tsx
+++ next-server/src/app/(chat)/layout.tsx
@@ -1,20 +1,3 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  metadataBase: new URL("https://www.devsonghee.com"),
-  title: {
-    default: "Scent Memories Chat",
-    template: "%s | Scent Memories Chat",
-  },
-  description: "향수를 좋아하는 사람들을 위한 소통 공간입니다.",
-  openGraph: {
-    title: "Scent Memories Chat",
-    description: "Socket.io 기반의 실시간 소통 공간",
-    url: "https://www.devsonghee.com/chatMember",
-    type: "website",
-  },
-};
-
 export default async function RootLayout({
   children,
 }: Readonly<{
