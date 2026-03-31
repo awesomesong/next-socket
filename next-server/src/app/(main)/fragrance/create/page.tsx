2de69b1e24dfca3d31f10d90093dad378b46ea22
songheekang <kshs24820@gmail.com>
2026-03-31 11:12:28 +0900

    fix(Layout): fragrance create 페이지의 min-h-screen 제거로 과도한 높이 확장 방지

--- next-server/src/app/(main)/fragrance/create/page.tsx
+++ next-server/src/app/(main)/fragrance/create/page.tsx
@@ -2,7 +2,7 @@ import FormFragrance from '@/src/app/components/FormFragrance';
 
 const FragranceCreatePage = () => {
     return (
-        <div className='px-4 pt-8 pb-4 md:p-8 max-w-[1440px] mx-auto min-h-screen'>
+        <div className='px-4 pt-8 pb-4 md:p-8 max-w-[1440px] w-full mx-auto'>
             <h2 className="text-top text-center">
                 <span className="text-gradient-scent">향수 정보 추가</span>
             </h2>
